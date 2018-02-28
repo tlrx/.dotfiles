933772cd8e5b80de276f177060ce0eb88f0f7062
Tanguy Leroux <tlrx.dev@gmail.com>
2018-02-28 15:02:48 +0100

    Enable company-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,23 @@
 (require 'package)
+
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (yaml-mode groovy-mode magit json-mode multi-term solarized-theme es-mode swiper company ivy find-file-in-repository))))
 
 (package-initialize)
 
@@ -25,7 +39,9 @@
 (delete-selection-mode 1)
 (setq inhibit-startup-message t)
 (setq read-file-name-completion-ignore-case t)
-(setq global-company-mode t)
+
+;; Company mode
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;; Ivy-mode
 (ivy-mode 1)
@@ -51,8 +67,8 @@
 ;; Search
 (global-set-key (kbd "C-s") 'swiper)
 
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-M-h") 'magit-log-buffer-file)
+;; Completion
+(global-set-key (kbd "C-c c") 'company-complete)
 
 ;; Windows
 (global-set-key (kbd "C-M-<right>") 'other-window)
@@ -61,24 +77,16 @@
 (global-set-key (kbd "C-S-v") 'split-window-vertically)
 (global-set-key (kbd "C-x k") 'kill-buffer-and-window)
 
+;; Others
 (global-set-key (kbd "C-S-d") 'speedbar)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-M-h") 'magit-log-buffer-file)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (solarized-dark)))
- '(custom-safe-themes
-   (quote
-    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(package-selected-packages
-   (quote
-    (yaml-mode groovy-mode magit json-mode multi-term solarized-theme es-mode swiper company ivy find-file-in-repository))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
